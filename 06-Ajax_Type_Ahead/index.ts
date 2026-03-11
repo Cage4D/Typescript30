86452bd696dbce88098410211fe8b2f7c6ab33a6
Johnny Cage <thespammingbott@gmail.com>
2026-03-11 22:39:08 +0100

    feat: implement city search with match highlighting and formatted population

--- 06-Ajax_Type_Ahead/index.ts
+++ 06-Ajax_Type_Ahead/index.ts
@@ -13,6 +13,32 @@ fetch(endpoint).then(data => data.json()).then((content:City[]) => cities.push(.
 function findMatches(wordToMatch:string, cities: City[]) {
     return cities.filter((place:City) => {
         const regex = new RegExp(wordToMatch, "gi")
-        return place.city.match(regex)
+        return place.city.match(regex) || place.state.match(regex)
     })
 }
+
+function numberWithCommas(x:Object) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, "")
+}
+
+function displayMatches(this:HTMLInputElement) {
+    const matchArray = findMatches(this.value, cities)
+    const html = matchArray.map(place => {
+        const regex = new RegExp(this.value, "gi")
+        const cityName = place.city.replace(regex, `<span class='hl'>${this.value}</span>`)
+        const stateName = place.state.replace(regex, `<span class='hl'>${this.value}</span>`)
+        return `
+            <li>
+                <span class="name">${cityName}, ${stateName}</span>
+                <span class="population">${numberWithCommas(place.population)}</span>
+            </li>
+        `
+    }).join(" ")
+
+    suggestions!.innerHTML = html
+}
+
+const searchInput = document.querySelector(".search")
+const suggestions = document.querySelector<HTMLUListElement>(".suggestions")
+
+searchInput?.addEventListener("change", displayMatches)
