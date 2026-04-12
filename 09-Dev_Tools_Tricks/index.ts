9d754c9be56bcc5d635fc35a501dbefd6082f729
Johnny Cage <thespammingbott@gmail.com>
2026-04-12 21:52:15 +0100

    .

--- 09-Dev_Tools_Tricks/index.ts
+++ 09-Dev_Tools_Tricks/index.ts
@@ -2,8 +2,8 @@ const dogs = [{ name: 'Snickers', age: 2 }, { name: 'hugo', age: 8 }];
 
     function makeGreen() {
       const p = document.querySelector<HTMLParagraphElement>('p');
-      p.style.color = '#BADA55';
-      p.style.fontSize = '50px';
+      p!.style.color = '#BADA55';
+      p!.style.fontSize = '50px';
     }
 
     // Regular
