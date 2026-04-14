d7897f61f71ad10cece3e9dd47ac1f50fb881433
Johnny Cage <thespammingbott@gmail.com>
2026-04-14 20:32:25 +0100

    implement handleCheck function that selects checkboxes in between the first and last when the shift key is held

--- 10-Hold-Shift-To-Check-Multiple-Checkboxes/index.ts
+++ 10-Hold-Shift-To-Check-Multiple-Checkboxes/index.ts
@@ -1,2 +1,23 @@
 const checkboxes = document.querySelectorAll<HTMLInputElement>(".inbox input[type='checkbox']")
-console.log(checkboxes)
+
+let lastChecked:HTMLInputElement | null = null;
+
+function handleCheck(e:MouseEvent) {
+    const target = e.currentTarget as HTMLInputElement;
+    
+    let inBetween:boolean = false;
+    if (e.shiftKey && target.checked) {
+        checkboxes.forEach(checkbox => {
+            if (checkbox === target || checkbox === lastChecked) {
+                inBetween = !inBetween;
+            }
+            if (inBetween) checkbox.checked = true;
+        })
+    }
+
+    lastChecked = target;
+}
+
+checkboxes.forEach(checkbox => {
+    checkbox.addEventListener("click", handleCheck)
+})
