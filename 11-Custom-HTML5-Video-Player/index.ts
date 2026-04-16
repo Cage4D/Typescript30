5aa862c59bdc38592fcae45ceb7ea401338f82df
Johnny Cage <thespammingbott@gmail.com>
2026-04-16 19:45:17 +0100

    Add video player DOM element references

---
+++ 11-Custom-HTML5-Video-Player/index.ts
@@ -0,0 +1,7 @@
+const player = document.querySelector<HTMLVideoElement>(".player")
+const video = player?.querySelector<HTMLVideoElement>(".viewer")
+const progress = player?.querySelector<HTMLDivElement>(".progress")
+const progressBar = player?.querySelector<HTMLDivElement>(".progress__filled")
+const toggle = player?.querySelector<HTMLButtonElement>(".toggle")
+const skipButtons = player?.querySelectorAll<HTMLButtonElement>("[data-skip]")
+const ranges = player?.querySelectorAll<HTMLInputElement>(".player__slider")
