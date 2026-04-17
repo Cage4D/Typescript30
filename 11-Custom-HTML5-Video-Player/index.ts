d9af27febb95091a0c96153685cf225c3a46faca
Johnny Cage <thespammingbott@gmail.com>
2026-04-17 14:48:33 -0700

    Implement togglePlay function for toggling video state between play and pause

--- 11-Custom-HTML5-Video-Player/index.ts
+++ 11-Custom-HTML5-Video-Player/index.ts
@@ -1,7 +1,20 @@
-const player = document.querySelector<HTMLVideoElement>(".player")
-const video = player?.querySelector<HTMLVideoElement>(".viewer")
-const progress = player?.querySelector<HTMLDivElement>(".progress")
-const progressBar = player?.querySelector<HTMLDivElement>(".progress__filled")
-const toggle = player?.querySelector<HTMLButtonElement>(".toggle")
-const skipButtons = player?.querySelectorAll<HTMLButtonElement>("[data-skip]")
-const ranges = player?.querySelectorAll<HTMLInputElement>(".player__slider")
+const player = document.querySelector<HTMLVideoElement>(".player");
+const video = player?.querySelector<HTMLVideoElement>(".viewer");
+const progress = player?.querySelector<HTMLDivElement>(".progress");
+const progressBar = player?.querySelector<HTMLDivElement>(".progress__filled");
+const toggle = player?.querySelector<HTMLButtonElement>(".toggle");
+const skipButtons = player?.querySelectorAll<HTMLButtonElement>("[data-skip]");
+const ranges = player?.querySelectorAll<HTMLInputElement>(".player__slider");
+
+function togglePlay() {
+  if (!video) return;
+
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+}
+
+video?.addEventListener("click", togglePlay);
+toggle?.addEventListener("click", togglePlay);
