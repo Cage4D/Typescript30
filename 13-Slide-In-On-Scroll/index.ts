0e98689b936c016727a479e56c7053ae2c64b8e4
Johnny Cage <thespammingbott@gmail.com>
2026-05-28 20:50:04 +0100

    feat: add slide-in scroll animation for images

--- 13-Slide-In-On-Scroll/index.ts
+++ 13-Slide-In-On-Scroll/index.ts
@@ -27,3 +27,25 @@ function debounce(
     }
   };
 }
+
+const sliderImages = document.querySelectorAll(".slide-in") as NodeListOf<HTMLImageElement>;
+
+function checkSlide() {
+  sliderImages.forEach((sliderImage) => {
+    const slideInAt =
+      window.scrollY + window.innerHeight - sliderImage.height / 2;
+
+    const imageBottom = sliderImage.offsetTop + sliderImage.height;
+
+    const isHalfShown = slideInAt > sliderImage.offsetTop;
+    const isNotScrolledPast = window.scrollY < imageBottom;
+
+    if (isHalfShown && isNotScrolledPast) {
+      sliderImage.classList.add("active");
+    } else {
+      sliderImage.classList.remove("active");
+    }
+  });
+}
+
+window.addEventListener("scroll", debounce(checkSlide));
