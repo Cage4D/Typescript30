6f7295af06cf51cd17313988cb0fe11b65930053
Johnny Cage <thespammingbott@gmail.com>
2026-06-05 20:09:23 +0100

    project end

--- 14-Javascript-References-vs-copying/index.ts
+++ 14-Javascript-References-vs-copying/index.ts
@@ -1,6 +1,16 @@
 const players = ['Wes', 'Sarah', 'Ryan', 'Poppy'];
 
+const team = players.slice();
+const team2 = [].concat(players);
+const team3 = [...players]
+
 const person = {
       name: 'Wes Bos',
       age: 80
 };
+
+const captain = person;
+captain.number = 99;
+
+const cap2 = Object.assign({}, person, { number: 99 })
+const cap3 = {...person}
